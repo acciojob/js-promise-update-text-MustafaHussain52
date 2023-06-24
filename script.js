cb61bf69e95c6a5f70e63564b759382715c7859b
Mustafa Hussain <118181312+MustafaHussain52@users.noreply.github.com>
2023-06-24 13:45:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function delayedHello() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('Hello, world!');
+    }, 1000);
+  });
+}
+
+const outputElement = document.getElementById('output');
+
+delayedHello()
+  .then((message) => {
+    outputElement.textContent = message;
+  })
+  .catch((error) => {
+    console.log('An error occurred:', error);
+  });
